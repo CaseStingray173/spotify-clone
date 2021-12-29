2f20d2c4cd09572ef0113be9fd83a0023b0cf4ca
Vraj Patel <patel.vraj3@gmail.com>
2021-12-29 10:49:18 -0500

    Added a few dispatches (will add more in future), and displaying the Player and Login components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import Login from "./Login"
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  // Acts as temporary storage, storage is cleared when reloaded
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+    }
+    // Empty [] means run the code only once
+  }, []);
+
   return (
     <div className="app">
-      <h1>Spotify-Clone</h1>
-      <Login />
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
